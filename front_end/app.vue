db2b2b6cd4626027fc17af9bddb432e29029a5ab
puzul <ymjonathangp2@gmail.com>
2024-09-10 11:06:40 +0700

    changed tables to grid

--- front_end/app.vue
+++ front_end/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col w-full min-h-screen">
+  <div class="flex flex-col w-full min-h-screen px-10 pt-10">
     <div class="flex justify-end">
       <button>
         Add
@@ -11,60 +11,65 @@
         Back
       </button>
     </div>
-    <table class="border border-solid border-gray-500">
-      <thead class="border border-solid border-gray-500">
-        <tr>
-          <th class="border border-solid border-gray-500">
-            First Name
-          </th>
-          <th class="border border-solid border-gray-500">
-            Last Name
-          </th>
-          <th class="border border-solid border-gray-500">
-            Position
-          </th>
-          <th class="border border-solid border-gray-500">
-            Phone
-          </th>
-          <th class="border border-solid border-gray-500">
-            Email
-          </th>
-        </tr>
-      </thead>
-      <tbody v-if="users" class="border border-solid border-gray-500">
-        <tr v-for="(user, index) in users" :key="`user-${user.id}`">
-          <td class="border border-solid border-gray-500" :class="{
-            'border-b-cyan-400' : editFieldArray[index].firstName
-          }">
-            <span v-if="!editFieldArray[index].firstName" @click="() => toggleEdit(editFieldArray[index], 'firstName', true, `inputFirstName${user.id}`)">
-              {{ user.firstName }}
-            </span>
-            <input v-else :id="`inputFirstName${user.id}`" type="text" :value="user.firstName" class="outline-none"
-              @focusout="() => toggleEdit(editFieldArray[index], 'firstName', false, `input${user.id}`)"
-              @change="event => user.firstName = (event.target as HTMLInputElement).value ">
-          </td>
-          <td class="border border-solid border-gray-500" :class="{
-            'border-b-cyan-400' : editFieldArray[index].lastName
-          }">
-            <span v-if="!editFieldArray[index].lastName" @click="() => toggleEdit(editFieldArray[index], 'lastName', true, `inputLastName${user.id}`)">
-              {{ user.lastName }}
-            </span>
-            <input v-else :id="`inputLastName${user.id}`" type="text" :value="user.lastName" class="outline-none"
-              @focusout="() => toggleEdit(editFieldArray[index], 'lastName', false, `inputLastName${user.id}`)"
-              @change="event => user.lastName = (event.target as HTMLInputElement).value ">
-          </td>
-          <td class="border border-solid border-gray-500">
-            {{ user.position }}
-          </td>
-          <td class="border border-solid border-gray-500">
-            {{ user.phone }}
-          </td>
-          <td class="border border-solid border-gray-500">
-            {{ user.email }}
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="grid grid-cols-5 border-x border-t border-solid border-gray-500" :class="{ 'border-b': !users }">
+      <div class="border-b border-solid border-gray-500 font-bold">
+        First Name
+      </div>
+      <div class="border-b border-solid border-gray-500 font-bold">
+        Last Name
+      </div>
+      <div class="border-b border-solid border-gray-500 font-bold">
+        Position
+      </div>
+      <div class="border-b border-solid border-gray-500 font-bold">
+        Phone
+      </div>
+      <div class="border-b border-solid border-gray-500 font-bold">
+        Email
+      </div>
+    </div>
+    <div v-if="users" v-for="(user, index) in users" :key="`user-${user.id}`" class="grid grid-cols-5 border-x border-solid border-gray-500">
+      <div class="border-b border-solid border-gray-500" :class="{ 'border-b-cyan-400' : editFieldArray[index].firstName }">
+        <span v-if="!editFieldArray[index].firstName" @click="() => toggleEdit(editFieldArray[index], 'firstName', true, `inputFirstName${user.id}`)">
+          {{ user.firstName }}
+        </span>
+        <input v-else :id="`inputFirstName${user.id}`" type="text" :value="user.firstName" class="outline-none"
+          @focusout="() => toggleEdit(editFieldArray[index], 'firstName', false, `input${user.id}`)"
+          @change="event => user.firstName = (event.target as HTMLInputElement).value ">
+      </div>
+      <div class="border-b border-solid border-gray-500"  :class="{ 'border-b-cyan-400' : editFieldArray[index].lastName }">
+        <span v-if="!editFieldArray[index].lastName" @click="() => toggleEdit(editFieldArray[index], 'lastName', true, `inputLastName${user.id}`)">
+          {{ user.lastName }}
+        </span>
+        <input v-else :id="`inputLastName${user.id}`" type="text" :value="user.lastName" class="outline-none"
+          @focusout="() => toggleEdit(editFieldArray[index], 'lastName', false, `input${user.id}`)"
+          @change="event => user.lastName = (event.target as HTMLInputElement).value ">
+      </div>
+      <div class="border-b border-solid border-gray-500" :class="{ 'border-b-cyan-400' : editFieldArray[index].position }">
+        <span v-if="!editFieldArray[index].position" @click="() => toggleEdit(editFieldArray[index], 'position', true, `inputPosition${user.id}`)">
+          {{ user.position }}
+        </span>
+        <input v-else :id="`inputPosition${user.id}`" type="text" :value="user.position" class="outline-none"
+          @focusout="() => toggleEdit(editFieldArray[index], 'position', false, `input${user.id}`)"
+          @change="event => user.position = (event.target as HTMLInputElement).value ">
+      </div>
+      <div class="border-b border-solid border-gray-500"  :class="{ 'border-b-cyan-400' : editFieldArray[index].phone }">
+        <span v-if="!editFieldArray[index].phone" @click="() => toggleEdit(editFieldArray[index], 'phone', true, `inputPhone${user.id}`)">
+          {{ user.phone }}
+        </span>
+        <input v-else :id="`inputPhone${user.id}`" type="text" :value="user.phone" class="outline-none"
+          @focusout="() => toggleEdit(editFieldArray[index], 'phone', false, `input${user.id}`)"
+          @change="event => user.phone = (event.target as HTMLInputElement).value ">
+      </div>
+      <div class="border-b border-solid border-gray-500"  :class="{ 'border-b-cyan-400' : editFieldArray[index].email }">
+        <span v-if="!editFieldArray[index].email" @click="() => toggleEdit(editFieldArray[index], 'email', true, `inputEmail${user.id}`)">
+          {{ user.email }}
+        </span>
+        <input v-else :id="`inputEmail${user.id}`" type="text" :value="user.email" class="outline-none"
+          @focusout="() => toggleEdit(editFieldArray[index], 'email', false, `input${user.id}`)"
+          @change="event => user.email = (event.target as HTMLInputElement).value ">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -115,7 +120,6 @@
       if (key==='phone') object.phone = true;
       if (key==='email') object.email = true;
       document.getElementById(inputId)?.focus();
-      console.log(object.lastName);
     }
     else {
       if (key==='firstName') object.firstName = false;
